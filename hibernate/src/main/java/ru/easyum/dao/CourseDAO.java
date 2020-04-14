1f3b1f415685bb86a18db48d70f3b31c2c59bb72
rsavenkov <lubajnka@mail.ru>
2020-04-15 01:31:53 +0300

    Lesson №7

---
+++ hibernate/src/main/java/ru/easyum/dao/CourseDAO.java
@@ -0,0 +1,27 @@
+package ru.easyum.dao;
+
+import org.springframework.stereotype.Repository;
+import ru.easyum.domain.Course;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class CourseDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Course findById(int id) {
+        return em.find(Course.class, id);
+    }
+
+    public List<Course> findAll() {
+        return em.createQuery("FROM Course").getResultList();
+    }
+
+    public void save(Course course) {
+        em.persist(course);
+    }
+}
